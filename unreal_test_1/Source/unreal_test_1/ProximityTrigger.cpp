7c9523617af5e0cbe87e32d2317ff83ab45d01d4
Madhusha Laksitha <67388969+MadhushaSUM@users.noreply.github.com>
2024-07-12 21:51:09 +0530

    Actor overlap trigger

--- unreal_test_1/Source/unreal_test_1/ProximityTrigger.cpp
+++ unreal_test_1/Source/unreal_test_1/ProximityTrigger.cpp
@@ -1,9 +1,18 @@
 #include "ProximityTrigger.h"
+#include "Components/CapsuleComponent.h"
 
 AProximityTrigger::AProximityTrigger()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
 
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(CapsuleComp);
+
+	CapsuleComp->OnComponentBeginOverlap.AddDynamic(this, &AProximityTrigger::OnOverlapStart);
 }
 
 void AProximityTrigger::BeginPlay()
@@ -18,3 +27,8 @@ void AProximityTrigger::Tick(float DeltaTime)
 
 }
 
+void AProximityTrigger::OnOverlapStart(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Something overlapped!!!"));
+}
--- unreal_test_1/Source/unreal_test_1/ProximityTrigger.h
+++ unreal_test_1/Source/unreal_test_1/ProximityTrigger.h
@@ -18,4 +18,14 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class UCapsuleComponent* CapsuleComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* Mesh;
+
+	UFUNCTION()
+	void OnOverlapStart(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+		bool bFromSweep, const FHitResult& SweepResult);
+
 };
